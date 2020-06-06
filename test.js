4d87bef723a1f5daade3f026b45655aa0cafe35a
nw <nico.walters@techtalents.es>
2020-06-06 11:25:29 +0200

    Slow async code

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 const fs = require("fs")
 
+function writeFileSlow() {
+    setTimeout(function() {
+        fs.writeFile("test2.txt", "", () => {})
+    }, 1000)
+}
+
 fs.writeFile("test1.txt", "", () => {})
-fs.writeFile("test2.txt", "", () => {})
+writeFileSlow()
 
 process.exit()
